187edc4762d93c851761ffcb8a091e8e1695d8d3
Jesús de Vicente Peña <devicentepena@webrtc.org>
2019-10-11 10:52:51 +0200

    Silk Decoder: lower the threshold for the DTX detection

--- silk/PLC.c
+++ silk/PLC.c
@@ -156,8 +156,11 @@ static OPUS_INLINE void silk_PLC_update(
         silk_memset( psPLC->LTPCoef_Q14, 0, LTP_ORDER * sizeof( opus_int16 ));
     }
 
-    /* If a no activity packet is received, check if it was a refresh DTX packet. */
-    psDec->inDTX = (psDec->indices.signalType == TYPE_NO_VOICE_ACTIVITY) && (psDec->lossCnt >= MAX_CONSECUTIVE_DTX);
+    /* If a no activity packet is received, check if it was a refresh DTX packet. The threshold in the
+     * loss counter is lower than MAX_CONSECUTIVE_DTX for covering the decoders that decide to not pull
+     * the whole duration of the DTX region.*/
+    psDec->inDTX = (psDec->indices.signalType == TYPE_NO_VOICE_ACTIVITY) &&
+                   (psDec->lossCnt >= MAX_CONSECUTIVE_DTX*75/100);
 
     /* Save LPC coeficients */
     silk_memcpy( psPLC->prevLPC_Q12, psDecCtrl->PredCoef_Q12[ 1 ], psDec->LPC_order * sizeof( opus_int16 ) );
